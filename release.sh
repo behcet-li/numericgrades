bb051e2cbbc94eb1e50d517757fada9efed1e942
Behcet <behcet@dolm.us>
2016-03-02 00:16:25 -0500

    Removed duplicate file. Updated release.sh

--- release.sh
+++ release.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
+rm release*.zip
+
 # chrome
-rm release.zip
 zip -r release_chrome.zip src icons js _locales manifest.json
 
 # firefox
-rm release_firefox.zip
 mkdir gecko
 cp -r src icons js _locales manifest.json gecko/
 cd gecko
@@ -13,4 +13,4 @@ cd gecko
 zip -r release_firefox.zip src icons js _locales manifest.json
 mv release_firefox.zip ../
 cd ..
-# rm -rf gecko
+rm -rf gecko
