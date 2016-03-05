40790b60c506e937cdb7b1f31d837f389613cadc
Behcet <behcet@dolm.us>
2016-03-05 12:34:34 -0500

    Updated release.sh

--- release.sh
+++ release.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
+RELEASE_DIRS="_locales html icons lib src manifest.json"
+
+echo $RELEASE_DIRS
+
 rm release*.zip
 
 # chrome
-zip -r release_chrome.zip src icons js _locales manifest.json
+zip -r release_chrome.zip $RELEASE_DIRS 
 
 # firefox
 mkdir gecko
-cp -r src icons js _locales manifest.json gecko/
+cp -r $RELEASE_DIRS gecko/
 cd gecko
 ../tools/manifest
-zip -r release_firefox.zip src icons js _locales manifest.json
-mv release_firefox.zip ../
-cd ..
+zip -r ../release_firefox.zip $RELEASE_DIRS
 rm -rf gecko
